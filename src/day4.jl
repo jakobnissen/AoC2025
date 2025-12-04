7ca64212c1474cb0ecc89b751c8f72e4721f8e99
Jakob Nybo Andersen <jakobnybonissen@gmail.com>
2025-12-04 14:14:26 +0100

    Optimize day 4

--- src/day4.jl
+++ src/day4.jl
@@ -6,64 +6,34 @@ using BufferIO; line_views
 import ..InputError, ..@nota
 
 # The eight directions of a cell
-const DELTAS = CartesianIndex.(
-    [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ]
-)
+const DELTAS = setdiff(CartesianIndices((-1:1, -1:1)), Ref(CartesianIndex(0, 0)))
 
-# Overall approach is to keep a list of indices which contain a roll that we need to
-# check if it can be removed.
-# For each roll we remove, we keep add its index to a set, and for each round of removal,
-# we report on the length of this set.
-
-# Then, for next round, we check all neighbors of the ones we removed.
 function solve(mem::ImmutableMemoryView{UInt8})
     M = @nota InputError parse(mem)
-    indices_to_check = collect(Iterators.filter(i -> M[i], CartesianIndices(M)))
-    indices_to_remove = Set{eltype(indices_to_check)}()
-    # Part 1 is only the first round
-    p1 = p2 = n_removed = remove_rolls!(M, indices_to_check, indices_to_remove)
-    while !iszero(n_removed)
-        # For part 2, keep removing rolls until we are done.
-        n_removed = remove_rolls!(M, indices_to_check, indices_to_remove)
-        p2 += n_removed
+    neighbour_count = Matrix{UInt8}(undef, size(M))
+    @inbounds for i in CartesianIndices(M)
+        M[i] || continue
+        neighbour_count[i] = count(d -> get(M, i + d, false), DELTAS) % UInt8
+    end
+    indices_to_remove = collect(Iterators.filter(i -> M[i] && neighbour_count[i] < 0x04, CartesianIndices(M)))
+    next_round = empty(indices_to_remove)
+    p1 = length(indices_to_remove)
+    p2 = 0
+    while !isempty(indices_to_remove)
+        empty!(next_round)
+        p2 += length(indices_to_remove)
+        @inbounds for i in indices_to_remove
+            M[i] = false
+            for d in DELTAS
+                get(M, i + d, false) || continue
+                old = neighbour_count[i + d]
+                neighbour_count[i + d] = old - 0x01
+                old == 0x04 && push!(next_round, i + d)
+            end
+        end
+        (indices_to_remove, next_round) = (next_round, indices_to_remove)
     end
     return (string(p1), string(p2))
-end
-
-function remove_rolls!(
-        M::BitMatrix,
-        indices_to_check::Vector{CartesianIndex{2}},
-        indices_to_remove::Set{CartesianIndex{2}},
-    )
-    for i in indices_to_check
-        neighbors = count(d -> get(M, i + d, false), DELTAS)
-        neighbors < 4 && push!(indices_to_remove, i)
-    end
-    # Add the neighbors of those we just removed to `indices_to_check`,
-    # since these may be eligible for removal in the next round.
-    empty!(indices_to_check)
-    n_removed = length(indices_to_remove)
-    for i in indices_to_remove
-        M[i] = false
-        for d in DELTAS
-            # In next round, only check those that have a roll, and which we
-            # are not removing in this round.
-            get(M, d + i, false) &&
-                !in(d + i, indices_to_remove) &&
-                push!(indices_to_check, d + i)
-        end
-    end
-    empty!(indices_to_remove)
-    return n_removed
 end
 
 # We parse it as a BitMatrix with @ being true.
