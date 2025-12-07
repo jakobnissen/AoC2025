49cb831baaa35b03bca987eddb02281ccf15f243
Jakob Nybo Andersen <jakobnybonissen@gmail.com>
2025-12-07 15:33:55 +0100

    Parse day6 properly

--- src/day6.jl
+++ src/day6.jl
@@ -4,23 +4,73 @@ using MemoryViews: MemoryView, ImmutableMemoryView
 using BufferIO: line_views
 using StringViews: StringView
 
-f(mul, it) = mul ? prod(it) : sum(it)
+import ..InputError, ..@nota
+
+@enum Operation::UInt8 mul add
+
+struct Block
+    horizontal::ImmutableMemoryView{Int}
+    vertical::ImmutableMemoryView{Int}
+    op::Operation
+end
+
+reduce(op::Operation, it::AbstractVector{Int}) = op == mul ? prod(it; init = 1) : sum(it; init = 0)
+reduce(b::Block) = (reduce(b.op, b.horizontal), reduce(b.op, b.vertical))
 
 function solve(mem::ImmutableMemoryView{UInt8})
-    lines = ImmutableMemoryView(collect(line_views(mem)))
-    ops = map(==("*"), eachsplit(StringView(last(lines))))
-    M = stack(lines[1:end-1], dims=1)
-    spaces = findall(map(i -> all(==(UInt8(' ')), i), eachcol(M)))
-    spans = [1:first(spaces)-1]
-    append!(spans, i+1:j-1 for (i,j) in zip(spaces, spaces[2:end]))
-    push!(spans, last(spaces)+1:size(M, 2))
-    p1 = p2 = 0
-    for (op, span) in zip(ops, spans)
-        S = M[:, span]
-        p1 += f(op, (Base.parse(Int, StringView(i)) for i in eachrow(S)))
-        p2 += f(op, (Base.parse(Int, StringView(i)) for i in eachcol(S)))
+    result = (0, 0)
+    for block in (@nota InputError parse(mem))
+        result = result .+ reduce(block)
     end
-    return (p1, p2)
+    return result
+end
+
+function parse(mem::ImmutableMemoryView{UInt8})::Union{Vector{Block}, InputError}
+    lines = ImmutableMemoryView(collect(line_views(mem)))
+    length(lines) < 2 && return InputError(nothing)
+
+    # Get a all lines but the last as matrix of bytes
+    M = stack(lines[1:(end - 1)], dims = 1)
+
+    # Get UnitRanges of the column indices that are not all spaces.
+    spaces = findall(map(i -> all(==(UInt8(' ')), i), eachcol(M)))
+    spans = [1:(first(spaces) - 1)]
+    append!(spans, (i + 1):(j - 1) for (i, j) in zip(spaces, spaces[2:end]))
+    push!(spans, (last(spaces) + 1):size(M, 2))
+
+    any(isempty, spans) && return InputError(nothing)
+    # For each horizontal span, we get the corresponding view of the matrix,
+    # then get the single op corresponding to that view, and parse the integers
+    # horizontally and vertically
+    opline = last(lines)
+    h_nums = Int[]
+    v_nums = Int[]
+    result = Block[]
+    for span in spans
+        # Parse operation - exactly one * or + in the final row
+        op = let
+            s = strip(StringView(opline[span]))
+            ncodeunits(s) == 1 || return InputError(nothing)
+            cu = codeunit(s, 1)
+            cu == UInt8('*') ? mul : (cu == UInt8('+') ? add : return InputError(length(lines)))
+        end
+
+        # Parse horizontally
+        S = view(M, :, span)
+        for row in eachrow(S)
+            push!(h_nums, Base.parse(Int, StringView(row)))
+        end
+        horizontal = ImmutableMemoryView(h_nums)[(end - size(S, 1) + 1):end]
+
+        # Parse vertically
+        for col in eachcol(S)
+            push!(v_nums, Base.parse(Int, StringView(col)))
+        end
+        vertical = ImmutableMemoryView(v_nums)[(end - size(S, 2) + 1):end]
+
+        push!(result, Block(horizontal, vertical, op))
+    end
+    return result
 end
 
 end # module Day6
